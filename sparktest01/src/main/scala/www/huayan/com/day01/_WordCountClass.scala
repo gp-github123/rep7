c6159ec214b542c0d37a38843ca9929c811dea2f
gp <13556677913@163.com>
2020-06-06 10:30:59 +0800

    wordcount

---
+++ sparktest01/src/main/scala/www/huayan/com/day01/_WordCountClass.scala
@@ -0,0 +1,15 @@
+package www.huayan.com.day01
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object _WordCountClass {
+  def main(args: Array[String]): Unit = {
+    val sc=new SparkContext(new SparkConf().setAppName("wordcount").setMaster("local[6]"))
+    sc.setLogLevel("warn")
+    sc.textFile("dataset/wordcount.txt")
+      .flatMap(_.split(" "))
+      .map((_,1))
+      .reduceByKey(_+_)
+      .foreach(println(_))
+  }
+}
